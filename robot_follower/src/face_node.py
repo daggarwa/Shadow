51a537a718ab28ea66b3edfe8e40ba29dd09443b
Mainak Biswas <mabiswas@eng.ucsd.edu>
2015-11-27 22:15:00 -0800

    face detection/recognition - working

---
+++ robot_follower/src/face_node.py
@@ -0,0 +1,217 @@
+#!/usr/bin/env python
+import roslib
+roslib.load_manifest('robot_follower')
+import sys
+import rospy
+import cv2
+from std_msgs.msg import String
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge, CvBridgeError
+import numpy as np
+import cPickle as pkl
+
+PATH = '/home/mainak/catkin_ws/src/robot_follower/src/'
+cascadePath = PATH+'haarcascade_frontalface_default.xml'
+#cascadePath = "src/robot_follower/src/haarcascade_frontalface_default.xml"
+face_cascade = cv2.CascadeClassifier(cascadePath)
+recognizer = cv2.createFisherFaceRecognizer()
+recognizer_lb = cv2.createLBPHFaceRecognizer()
+TRAIN_SIZE = 800
+TEST_SIZE = 200
+
+class face_node:
+
+  def __init__(self):
+    self.image_pub = rospy.Publisher("image_topic_2",Image, queue_size=10)
+    self.is_image_present = 0
+
+    self.bridge = CvBridge()
+    #self.image_sub = rospy.Subscriber("/usb_cam/image_raw",Image,self.callback)
+    self.image_sub = rospy.Subscriber("/camera/rgb/image_color",Image,self.callback)
+
+  def callback(self,data):
+    try:
+      cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
+    except CvBridgeError as e:
+      print(e)
+
+    self.image = cv_image
+    #self.gray_image = cv2.cvtColor(cv_image, cv2.COLOR_BGR2GRAY)
+    self.is_image_present = 1
+
+  def detect_face(self):
+
+    height = 480
+    width = 640
+    rate = rospy.Rate(10) # 10hz
+    cv2.namedWindow("Image window", 1)
+
+    while not rospy.is_shutdown():
+      if(self.is_image_present):
+        im = self.image
+      else:
+        im = np.ones((height,width,3), np.uint8)
+      #im = cv2.imread('sachin.jpg')
+      (rows,cols,channels) = im.shape
+      im_gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+      #(rows,cols) = im.shape
+      faces = face_cascade.detectMultiScale(im_gray, 1.3, 5)
+      for (x,y,w,h) in faces:
+        #print 'face found !!!!!!!!'
+        cv2.rectangle(im,(x,y),(x+w,y+h),(255,0,0),2)
+        face_gray = np.array(im_gray[y:y+h, x:x+w], 'uint8')
+
+      cv2.imshow("Image window", im)
+      cv2.waitKey(3)
+
+      try:
+        self.image_pub.publish(self.bridge.cv2_to_imgmsg(im, "bgr8"))
+      except CvBridgeError as e:
+        print(e)
+      rate.sleep()
+
+  def prepare_face_data(self, face_label):
+
+    height = 480
+    width = 640
+    rate = rospy.Rate(10) # 10hz
+    facecount = 0
+    face_images = []
+    face_labels = []
+    cv2.namedWindow("Training Set Preparation", 1)
+
+    while (not rospy.is_shutdown()) and (facecount < TRAIN_SIZE+TEST_SIZE):
+      if(self.is_image_present):
+        im = self.image
+      else:
+        im = np.ones((height,width,3), np.uint8)
+      #im = cv2.imread('sachin.jpg')
+      #print 'image received !!!'
+      (rows,cols,channels) = im.shape
+      im_gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+      #(rows,cols) = im.shape
+      faces = face_cascade.detectMultiScale(im_gray, 1.3, 5)
+      #print 'face detected !!!'
+      if (len(faces) == 1):
+        for (x,y,w,h) in faces:
+          #print 'face found !!!!!!!!'
+          if ((w>100) and (h>100)):
+            cv2.rectangle(im,(x,y),(x+w,y+h),(255,0,0),2)
+            face_gray = np.array(im_gray[y:y+h, x:x+w], 'uint8')
+            face_sized = cv2.resize(face_gray, (200, 200))
+            #face_gray = im_gray[y:y+h, x:x+w]
+            face_images.append(face_sized)
+            face_labels.append(face_label)
+            facecount += 1
+            #print 'Image Size: ', w, h
+      cv2.imshow("Training Set Preparation", im)
+      cv2.waitKey(3)
+      try:
+        self.image_pub.publish(self.bridge.cv2_to_imgmsg(im, "bgr8"))
+      except CvBridgeError as e:
+        print(e)
+      rate.sleep()
+
+    cv2.destroyWindow("Training Set Preparation")
+    return face_images, face_labels
+
+  def recognize_faces(self, names):
+
+    height = 480
+    width = 640
+    rate = rospy.Rate(10) # 10hz
+    cv2.namedWindow("Recognize Face", 1)
+
+    while not rospy.is_shutdown():
+      if(self.is_image_present):
+        im = self.image
+      else:
+        im = np.ones((height,width,3), np.uint8)
+      (rows,cols,channels) = im.shape
+      im_gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+      #(rows,cols) = im.shape
+      faces = face_cascade.detectMultiScale(im_gray, 1.3, 5)
+      #if (len(faces) == 1):
+      for (x,y,w,h) in faces:
+        if ((w>100) and (h>100)):
+          cv2.rectangle(im,(x,y),(x+w,y+h),(255,0,0),2)
+          face_gray = np.array(im_gray[y:y+h, x:x+w], 'uint8')
+          face_sized = cv2.resize(face_gray, (200, 200))
+          #face_predicted, conf = recognizer.predict(face_sized)
+          face_predicted, conf = recognizer_lb.predict(face_sized)
+          print 'This is ', names[face_predicted]
+          print 'Confidence: ', conf 
+
+      cv2.imshow("Recognize Face", im)
+      cv2.waitKey(3)
+      try:
+        self.image_pub.publish(self.bridge.cv2_to_imgmsg(im, "bgr8"))
+      except CvBridgeError as e:
+        print(e)
+      rate.sleep()
+
+def main(args):
+  ic = face_node()
+  rospy.init_node('face_node', anonymous=True)
+  count = 0
+  label_names = []
+  print 'TRAIN SIZE: ', TRAIN_SIZE
+  ans = raw_input('Train faces?  ')
+  if (ans == 'yes'):
+    name = raw_input('Name of the person:  ')
+    train_faces, train_labels = ic.prepare_face_data(count);
+    label_names.append(name)
+    while(ans=='yes'):
+      ans = raw_input('Do you want to train the model with some more guys?  ')
+      if (ans == 'yes'):
+        count += 1
+        name = raw_input('Name of the person:  ')
+        train_faces_new, train_labels_new = ic.prepare_face_data(count);
+        label_names.append(name)
+        train_faces = train_faces + train_faces_new
+        train_labels = train_labels + train_labels_new
+      elif(ans == 'no'):
+        break
+    data = [train_faces, train_labels, label_names]
+    with open(PATH+'facedata.pkl', 'wb') as output:
+      pkl.dump(data, output)
+
+  with open(PATH+'facedata.pkl', "rb") as data:
+    datasets = pkl.load(data)
+  faces, labels, names = datasets
+  #train_faces = faces[:TRAIN_SIZE]
+  #train_faces = faces[:TRAIN_SIZE]
+  #train_faces = faces[:TRAIN_SIZE]
+  
+  #labels = []
+  #for i in range(100):
+  #  labels.append(int(0))
+  #for i in range(100):
+  #  labels.append(int(1))
+  #for i in range(100):
+  #  labels.append(int(2))
+  #print 'Length: ', len(labels)
+
+  #train_faces = []
+  #train_labels = []
+  #for face in faces:
+  #  train_faces.append(np.array(face, 'uint8'))
+  #print 'TRAIN LENGTH: ', len(train_faces)
+  #print labels
+  #for label in labels:
+  #  train_labels.append(int(label))
+
+  #recognizer.train(faces, np.array(labels))
+  recognizer_lb.train(faces, np.array(labels))
+
+  print 'Dataset Prepared!!!'
+  #if (ans == 'no'):
+  ic.recognize_faces(names)
+  try:
+    rospy.spin()
+  except KeyboardInterrupt:
+    print("Shutting down")
+  cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+    main(sys.argv)
